9be69468bf4cd4fef6fe2af9b42ddf7729d9e077
Javen Peng <penguinleo@163.com>
2019-01-24 20:28:11 +0800

    the error was corrected

--- Julian.py
+++ Julian.py
@@ -1,15 +1,15 @@
 def Julian(Date):
-    import math
-# this code was translated from matlab
-    year    = Date['year']
-    month   = Date['month']
-    day     = Date['day']
-    hour    = Date['hour']
-    minute  = Date['minute']
-    second  = Date['second']
-    print (Date)
-# this function calculate the Julian day number at 0 UT for any year between 1900 and 2100
-
-    day     = day + (hour + minute/60 + second/3600) / 24
-    j0      = 365*year - int(7*(year + int((month + 9)/12))/4) + int(275*month/9) + day + 1721013.5
-    return j0
+    # Date = {"year":2018,"month":12,"day":29,"hour":19,"minute":16,"second":00,"ms":0}
+    print(Date)
+    year    = Date["year"]
+    month   = Date["month"]
+    day     = Date["day"]
+    hour    = Date["hour"]
+    minute  = Date["minute"]
+    second  = Date["second"]
+    ms      = Date["ms"]
+    day     = day + (hour + minute/60 + second/3600 + ms/3600000) / 24
+    # j0      = 367*year - int(7*(year + int((month + 9)/12))/4)  + int(275*month/9) + day + 1721013.5
+    j0      = 367*year - int(7*(year + int((month + 9)/12))/4)  + int(275*month/9) + day + 1721013.5
+    j1      = j0 * 24 * 3600
+    return j1
